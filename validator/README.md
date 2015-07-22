bfba86b4d18903d212e9c11a5f132c102b476c18
Daniel Meißner <meise@koeln.ccc.de>
2015-07-23 01:04:23 +0200

    added schedule xml xsd file plus easy to use bash script

---
+++ validator/README.md
@@ -0,0 +1,14 @@
+# schedule.xml validate
+
+Validates given schedule.xml against xsd file in same folder.
+
+## Usage
+
+```
+ ./validate_schedule_xml.sh https://programm.froscon.de/2015/schedule.xml
+```
+
+## Requirements
+
+  * xmllint
+  * curl
---
+++ validator/validate_schedule_xml.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+xmllint_cmd=`command -v xmllint`
+curl_cmd=`command -v curl`
+xsd_file=`dirname $0`/schedule.xml.xsd
+
+if [ -z "${xmllint_cmd}" ]; then
+  echo "Please install xmllint!"
+  exit 1
+fi
+
+if [ -z "${curl_cmd}" ]; then
+  echo "Please install curl!"
+fi
+
+if [ -z "${1}" ]; then
+  echo "Please provide schedule xml http(s) URL."
+  echo "  ${0} http://example.com/schedule.xml"
+  exit 1
+fi
+
+if [ ! -e "${xsd_file}" ]; then
+  echo "schedule.xml.xsd missing!"
+  exit 1
+fi
+
+$curl_cmd $1 2>/dev/null | $xmllint_cmd --noout --schema ${xsd_file} -
+
+xmllint_err=$?
+if [ 0 -eq $xmllint_err ]; then
+  echo
+  echo "Yeeeeeah… ${1} validates ${xsd_file}!!1!"
+fi
+
+exit $?
