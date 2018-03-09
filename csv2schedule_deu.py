07ce9ceffcfa0b8da1eea6dae67c9fb0ad75a5a1
Andreas Hubel <andi@saerdnaer.de>
2018-03-09 23:25:04 +0100

    whitespace cleanup

--- csv2schedule_deu.py
+++ csv2schedule_deu.py
@@ -56,7 +56,7 @@ else:
 
 
 # specifies the date format used in the CSV file respectivly the google docs spreadsheet
-date_format = '%Y-%m-%d %H:%M' 
+date_format = '%Y-%m-%d %H:%M'
 default_talk_length = timedelta(minutes=args.default_talk_length)
 # end config
 
@@ -64,7 +64,7 @@ default_talk_length = timedelta(minutes=args.default_talk_length)
 template = { "schedule":  OrderedDict([
         ("version", "1.0"),
         ("conference",  OrderedDict([
-            ("title", ""), 
+            ("title", ""),
             ("acronym", acronym),
             ("daysCount", 1),
             ("start", ""),
@@ -76,7 +76,7 @@ template = { "schedule":  OrderedDict([
 }
 
 if not os.path.exists(output_dir):
-    os.mkdir(output_dir) 
+    os.mkdir(output_dir)
 os.chdir(output_dir)
 
 
@@ -85,18 +85,18 @@ def main():
 
 def process(acronym, base_id, source_csv_url):
     out = template
-    
+
     print('Processing ' + acronym)
-    
+
     if not offline:
         print(" requesting schedule source from url")
         schedule_r = requests.get(source_csv_url)
         # don't ask me why google docs announces by header? that it will send latin1 and then sends utf8...
         schedule_r.encoding = 'utf-8'
-        
+
         if schedule_r.ok is False:
             raise Exception("  Requesting schedule from CSV source url failed, HTTP code {0} from {1}.".format(schedule_r.status_code, source_csv_url))
-        
+
         with open('schedule-' + acronym + '.csv', 'w') as f:
             f.write(schedule_r.text)
 
@@ -104,16 +104,16 @@ def process(acronym, base_id, source_csv_url):
     csv_schedule = []
     max_date = None
     min_date = None
-    
+
     filename = 'schedule-' + acronym + '.csv'
-    if sys.version_info[0] < 3: 
+    if sys.version_info[0] < 3:
         infile = open(filename, 'rb')
     else:
         infile = open(filename, 'r', newline='', encoding='utf8')
 
     with infile as f:
         reader = csv.reader(f)
-        
+
         # first header
         keys = next(reader)
         # store conference title from top left cell into schedule
@@ -122,11 +122,11 @@ def process(acronym, base_id, source_csv_url):
         last = keys[0] = 'meta'
         keys_uniq = []
         for i, k in enumerate(keys):
-            if k != '': 
+            if k != '':
                 last = k.strip()
                 keys_uniq.append(last)
             keys[i] = last
-        
+
         # second header
         keys2 = next(reader)
 
@@ -136,7 +136,7 @@ def process(acronym, base_id, source_csv_url):
             i = 0
             items = OrderedDict([ (k, OrderedDict()) for k in keys_uniq ])
             row_iter = iter(row)
-            
+
             for value in row_iter:
                 value = value.strip()
                 if keys2[i] != '' and value != '':
@@ -146,38 +146,39 @@ def process(acronym, base_id, source_csv_url):
                       print("Error in row {}, cell {} value: {}".format(keys[i],keys2[i], value))
                       raise e
                 i += 1
-            
+
             try:
-              start_time = datetime.strptime( items['meta']['Datum'] + ' ' + items['meta']['Uhrzeit'], date_format)
-              items['start_time'] = start_time
-              items['end_time'] = start_time + default_talk_length
-              
-              # only accept valid entries
-              if len(items['meta']) > 0 and 'Titel' in items['meta']:
+                start_time = datetime.strptime( items['meta']['Datum'] + ' ' + items['meta']['Uhrzeit'], date_format)
+                items['start_time'] = start_time
+                items['end_time'] = start_time + default_talk_length
+
+                # only accept valid entries
+                if len(items['meta']) > 0 and 'Titel' in items['meta']:
                   csv_schedule.append(items)
-                  
+
                   if min_date is None or start_time < min_date:
                       min_date = start_time
                   if max_date is None or start_time > max_date:
                       max_date = start_time
-                  
-                  # check if end_time of previous event (calculated from default_talk_length) overlaps with start_time 
+
+                  # check if end_time of previous event (calculated from default_talk_length) overlaps with start_time
                   # long term TODO: check also other events not only the previous one
                   if last is not None and last['end_time'] > start_time:
                       last['end_time'] = start_time
                   last = items
-              else:
-                print(" ignoring empty/invalid row in CSV file")
-            except:
-              print(" ignoring row with invalid date in CSV file")
-    
-    #print json.dumps(csv_schedule, indent=4) 
-    
-    
+                else:
+                    print(" ignoring empty/invalid row in CSV file")
+            except RuntimeError as e:
+                print(" ignoring row with invalid date in CSV file")
+                print(e)
+
+    #print json.dumps(csv_schedule, indent=4)
+
+
     out['schedule']['conference']['start'] = min_date.strftime('%Y-%m-%d')
     out['schedule']['conference']['end'] = max_date.strftime('%Y-%m-%d')
     out['schedule']['conference']['daysCount'] = (max_date - min_date).days + 1
-    
+
     print(" converting to schedule ")
     conference_start_date = dateutil.parser.parse(out['schedule']['conference']['start'])
 
@@ -185,7 +186,7 @@ def process(acronym, base_id, source_csv_url):
         date = conference_start_date + timedelta(days=i)
         start = date + timedelta(hours=10) # conference day starts at 10:00
         end = start + timedelta(hours=17)  # conference day lasts 17 hours
-        
+
         out['schedule']['conference']['days'].append(OrderedDict([
             ('index', i),
             ('date' , date.strftime('%Y-%m-%d')),
@@ -193,16 +194,16 @@ def process(acronym, base_id, source_csv_url):
             ('end', end.isoformat()),
             ('rooms', OrderedDict())
         ]))
-    
+
     for event in csv_schedule:
         id = str(base_id + int(event['meta']['ID']))
         room = event['meta']['Raum']
         guid = voc.tools.gen_uuid(hashlib.md5((acronym + id).encode('utf-8')).hexdigest())
         duration = (event['end_time'] - event['start_time']).seconds/60
 
-	if args.split_persons:
-	    event['Vortragende'] = event['Vortragende'].split(',')
-        
+        if args.split_persons:
+            event['Vortragende'] = event['Vortragende'].split(',')
+
         event_n = OrderedDict([
             ('id', id),
             ('guid', guid),
@@ -227,29 +228,26 @@ def process(acronym, base_id, source_csv_url):
             ]) for p in event['Vortragende'].values() ]),
             ('links', [])
         ])
-        
+
         #print event_n['title']
-        
+
         day = (event['start_time'] - conference_start_date).days + 1
         day_rooms = out['schedule']['conference']['days'][day-1]['rooms']
         if room not in day_rooms:
             day_rooms[room] = list()
         day_rooms[room].append(event_n)
-        
-        
-    
+
     #print json.dumps(schedule, indent=2)
-    
+
     print(" writing results to disk")
     with open('schedule-' + acronym + '.json', 'w') as fp:
         json.dump(out, fp, indent=4)
-        
+
     with open('schedule-' + acronym + '.xml', 'w') as fp:
         fp.write(voc.tools.dict_to_schedule_xml(out));
-    
+
     # TODO: Validate XML via schema file
     print(' end')
-    
 
 
 if __name__ == '__main__':
