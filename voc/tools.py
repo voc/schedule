b0199d7da2a91dde5d64f6c90ce1bc02bf3c4534
Tobias Preuss <tobias.preuss@googlemail.com>
2021-03-31 23:15:32 +0200

    Improve maintenance by reducing PEP 8 warnings.
    + Fix a few spelling mistakes.

--- voc/tools.py
+++ voc/tools.py
@@ -26,20 +26,23 @@ def set_base_id(value):
 def get_id(guid):
     global sos_ids, next_id, generated_ids
     if guid not in sos_ids:
-        #generate new id
+        # generate new id
         sos_ids[guid] = next_id
         next_id += 1
         generated_ids += 1
 
     return sos_ids[guid]
 
+
 def gen_random_uuid():
     return uuid.uuid4()
+
 
 def gen_uuid(name):
     return str(uuid.uuid5(uuid_namespace, str(name)))
 
-# depreacated, use Schedule.foreach_event() instead
+
+# deprecated, use Schedule.foreach_event() instead
 # TODO remove
 def foreach_event(schedule, func):
     out = []
@@ -63,6 +66,7 @@ def copy_base_structure(subtree, level):
                 ret[key] = copy_base_structure(value, level-1)
     return ret
 
+
 def copy_base_structure_list(subtree, level):
     ret = []
     if level > 0:
@@ -82,21 +86,23 @@ def normalise_string(string):
     string = string.replace(u'ö', 'oe')
     string = string.replace(u'ü', 'ue')
     string = string.replace(u'ß', 'ss')
-    string = re.sub('\W+', '\_', string.strip()) # replace whitespace with _
+    string = re.sub('\W+', '\_', string.strip())  # replace whitespace with _
     # string = filter(unicode.isalnum, string)
     string = re.sub('[^a-z0-9_]+', '', string) # TODO: is this not already done with \W+  line above?
-    string = string.strip('_') # remove trailing _
+    string = string.strip('_')  # remove trailing _
 
     return string
+
 
 def normalise_time(timestr):
     timestr = timestr.replace('p.m.', 'pm')
     timestr = timestr.replace('a.m.', 'am')
-    ## workaround for failure in input file format
+    # workaround for failure in input file format
     if timestr.startswith('0:00'):
         timestr = timestr.replace('0:00', '12:00')
 
     return timestr
+
 
 def parse_json(text):
     # this more complex way is necessary
@@ -106,9 +112,8 @@ def parse_json(text):
 
 def load_json(filename):
     with open(filename, "r") as fp:
-        #data = json.load(fp) 
+        # data = json.load(fp)
         # maintain order from file
         data = parse_json(fp.read())
         
     return data
-    
